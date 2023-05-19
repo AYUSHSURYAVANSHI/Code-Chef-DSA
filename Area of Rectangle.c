6359988b3671c21ecd8e3ecd6bc81b56c790e4e4
Ayush Suryavanshi <113771722+AYUSHSURYAVANSHI@users.noreply.github.com>
2023-05-19 21:47:11 +0530

    Create Area of Rectangle.c

---
+++ Area of Rectangle.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+  int length = 45 ; 
+  int width = 76 ; 
+  int area = length * width;
+  printf("The Area of the given rectangle is %d", area ); 
+  return 0;
+}
